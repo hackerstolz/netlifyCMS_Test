f357cc28468524dc6f3e458a7fbb29cf6837ba3a
Sascha Ulbrich <sascha.ulbrich@hackerstolz.de>
2020-01-25 21:13:27 +0100

    Netlify Retest

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,36 +5,87 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  transformers: {
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        // ...global plugins
-      ]
-    }
-  },
+    siteName: 'Gridsome',
 
-  plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'content/**/*.md',
-        typeName: 'Test',
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
+    templates: {
+       Hackathon: '/:title'
+    },
+
+    transformers: {
+      remark: {
+        externalLinksTarget: '_blank',
+        externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+        anchorClassName: 'icon icon-link',
+        plugins: [
+          '@gridsome/remark-prismjs'
+        ]
+      }
+    },
+    plugins: [
+      {
+        use: '@gridsome/source-filesystem',
+        options: {
+          path: 'content/hackathons/**/*.md',
+          typeName: 'Hackathon',
+          refs: {
+                    // Creates a GraphQL collection from challenges in front-matter and adds a reference.
+                    challenges: {
+                      typeName: 'Challenge'//,
+                      //create: true
+                    }
+                  },
+          remark: {
+            plugins: [
+              // ...local plugins
+            ]
+          }
         }
-      }
-    },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        publicPath: `/admin`
-      }
-    },
-  ]
+      },
+      {
+          use: '@gridsome/source-filesystem',
+          options: {
+            path: 'content/challenges/**/*.md',
+            typeName: 'Challenge',
+            refs: {
+                    },
+            remark: {
+              plugins: [
+                // ...local plugins
+              ]
+            }
+          }
+      },
+      {
+          use: 'gridsome-plugin-netlify-cms',
+          options: {
+            publicPath: '/admin',
+            modulePath: 'src/admin/index.js',
+            configPath: 'src/admin/config.yml',
+            htmlPath: 'src/admin/index.html',
+            htmlTitle: 'My Gridsome integrated CMS'
+          }
+      }/*,
+      {
+          use: 'gridsome-plugin-netlify-cms-paths'
+      }*/
+    ]
+
+/*    plugins: [
+        {
+          use: '@gridsome/source-strapi',
+          options: {
+            apiURL: 'http://localhost:1337',
+            queryLimit: 1000, // Defaults to 100
+            contentTypes: ['Hackathon'],
+            // Possibility to login with a Strapi user,
+            // when content types are not publicly available (optional).
+            // loginData: {
+            //   identifier: '',
+            //   password: ''
+            //}
+          }
+        }
+      ]
+ */
+
 }
--- posts/title1.md
+++
@@ -1,6 +0,0 @@
----
-title: Title1
-excerpt: Excerpt1
-date: 2020-01-23T18:31:22.689Z
----
-Text1
