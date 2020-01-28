179b3992b0389f025be08e5eda069bb25b4ef8b6
Sascha Ulbrich (valantic) <sascha.ulbrich@cec.valantic.com>
2020-01-28 23:31:20 +0100

    ID issue

--- src/admin/index.js
+++ src/admin/index.js
@@ -24,11 +24,25 @@ const IdControl = window.createClass({
     },
 
 	componentDidUpdate: function() {
+		const regString = " (regenerated) ";
         // If this widget doesn't have an ID yet we create one using the UUID package  
-		if(!this.props.value.includes("NEW")){
-		this.props.onChange(this.props.value + "NEW");
+		if(this.props.value.includes(regString)){ //it was just regenerated - update it silently (without update -> else endless loop)
+		//if (this.props.uniqueFieldId == "NEW"){
+			//this.props.uniqueFieldId = "undefined";
+			this.props.value = this.props.value.replace(regString, "");
+		}else{ //seems to be copy case -> regenerated 
+			//this.props.regen = "X";
+			//this.props.uniqueFieldId = uuid();
+			//this.props.uniqueFieldId = "NEW";
+			this.props.onChange(uuid() + regString);
+			
 		}
-    }
+    }/*,
+	getSnapshotBeforeUpdate: function(){
+		if(prevProps.value == this.props.value){ //no change -> lets trigger change of value
+			this.props.uniqueFieldId = uuid();
+		}		
+	}*/
 });
 
 /**
